7233451ecd608223eda8ad1e7df46e3776a036e3
Dima Dolgov <90906120+dimadolgov@users.noreply.github.com>
2023-12-20 11:23:08 +0200

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,37 +1,39 @@
 # configure haproxy instance 
 resource "aws_instance" "ec2_haproxy" {
-    count                       = "1"
-    ami                         = var.ami
-    instance_type               = var.instance_type
-    associate_public_ip_address = true
-    key_name                    = "SSH"
-    subnet_id                   = aws_subnet.subnet_public_web_haproxy.id
-    vpc_security_group_ids      = [aws_security_group.web_haproxy.id]
-    depends_on                  = [aws_instance.ec2_web_server]
-    user_data                   = templatefile("haproxy.tpl",{
-    instance_ip                 = join(",", aws_instance.ec2_web_server.*.private_ip)
-    instance_name               = join(",", aws_instance.ec2_web_server.*.tags.Name)
-     })
-    tags = {
+  count                       = 1
+  ami                         = var.ami
+  instance_type               = var.instance_type
+  associate_public_ip_address = true
+  key_name                    = "SSH"
+  subnet_id                   = aws_subnet.subnet_public_web_haproxy.id
+  vpc_security_group_ids      = [aws_security_group.web_haproxy.id]
+  depends_on                  = [aws_instance.ec2_web_server]
+
+  user_data = templatefile("haproxy.tpl", {
+    instance_ip   = join(",", aws_instance.ec2_web_server.*.private_ip)
+    instance_name = join(",", aws_instance.ec2_web_server.*.tags.Name)
+  })
+
+  tags = {
     Name = "haproxy"
-    }
+  }
 }
 
 # configure web server instance 
 resource "aws_instance" "ec2_web_server" {
-    count                       = "2"
-    ami                         = var.ami
-    instance_type               = var.instance_type
-    associate_public_ip_address = false
-    key_name                    = "SSH"
-    subnet_id                   = aws_subnet.subnet_public_web_haproxy.id
-    vpc_security_group_ids      = [aws_security_group.web_haproxy.id]
+  count                       = var.ec2_count
+  ami                         = var.ami
+  instance_type               = var.instance_type
+  associate_public_ip_address = true
+  key_name                    = "SSH"
+  subnet_id                   = aws_subnet.subnet_public_web_haproxy.id
+  vpc_security_group_ids      = [aws_security_group.web_haproxy.id]
 
-    user_data = templatefile("web_server.tpl", { })
+  user_data = templatefile("web_server.tpl", {})
 
-    tags = {
-        Name = "web_${format("%02d", count.index + 1)}"
-    }
+  tags = {
+    Name = "web_${format("%02d", count.index + 1)}"
+  }
 }
 
 

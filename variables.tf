59f0e22945ced1d9e66297b5e4029cb99a367de9
Dima Dolgov <90906120+dimadolgov@users.noreply.github.com>
2023-12-26 21:07:17 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,42 +1,42 @@
 ############ vpc + subnets ############
 variable "region" {
-    type = string
-    default = "il-central-1"
+  type    = string
+  default = "il-central-1"
 }
 variable "vpc_cidr_block" {
-    type = string
-    default = "20.20.0.0/16"
+  type    = string
+  default = "20.20.0.0/16"
 }
 variable "subnet_cidr_public" {
-    type = string
-    default = "20.20.200.0/24"
+  type    = string
+  default = "20.20.200.0/24"
 }
 
 ############ security groups ############
 variable "ingress_cidr_blocks" {
-    type = list
-    default = ["0.0.0.0/0"]
-    description = "ingress_cidr_blocks"
+  type        = list(any)
+  default     = ["0.0.0.0/0"]
+  description = "ingress_cidr_blocks"
 }
 variable "ingres_ports" {
-    type = list 
-    default = ["80","22"]
+  type    = list(any)
+  default = ["80", "22"]
 }
 variable "egress_ports" {
-    type = list
-    default = ["0.0.0.0/0"]
+  type    = list(any)
+  default = ["0.0.0.0/0"]
 }
 
 ############ ec2 instances ############
 variable "instance_type" {
-    type = string
-    default = "t3.nano"
+  type    = string
+  default = "t3.nano"
 }
 variable "ami" {
-    type = string
-    default = "ami-0f57fcd358b56d855"
+  type    = string
+  default = "ami-0f57fcd358b56d855"
 }
 variable "ec2_count" {
-    type = string
-    default = "5"
+  type    = string
+  default = "6"
 }

f0aed2636dbc82cce951b7f4f6ade4d71133203d
Dima Dolgov <90906120+dimadolgov@users.noreply.github.com>
2023-12-26 21:07:02 +0200

    Update security_group.tf

--- security_group.tf
+++ security_group.tf
@@ -4,7 +4,7 @@ resource "aws_security_group" "web_haproxy" {
   description = "dynamic"
   vpc_id      = aws_vpc.vpc_web_haproxy.id # specify the vpc id where the security group belongs
 
-# dynamic security group (inbound rules)
+  # dynamic security group (inbound rules)
   dynamic "ingress" {
     for_each = var.ingres_ports
     content {
@@ -16,15 +16,23 @@ resource "aws_security_group" "web_haproxy" {
     }
   }
 
- # allow all outgoing traffic (outbound rules)
+  # llow traffic from the security group itself
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    self      = true
+  }
+
+  # allow all outgoing traffic (outbound rules)
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = "-1"          # -1 indicates all protocols
-    cidr_blocks = var.egress_ports  # allow traffic to all destinations
+    protocol    = "-1"             # -1 indicates all protocols
+    cidr_blocks = var.egress_ports # allow traffic to all destinations
   }
   # assign tags to the security group for better identification
   tags = {
-    Name = "terraform_sg_dynamic"
+    Name = "Security_Group_HAProxy"
   }
 }

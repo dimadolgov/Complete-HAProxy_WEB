297e9d476d59dad47a64d5f8acc26eb1c17be8e2
Dima Dolgov <90906120+dimadolgov@users.noreply.github.com>
2023-12-26 21:06:23 +0200

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -4,13 +4,13 @@ resource "aws_instance" "ec2_haproxy" {
   ami                         = var.ami
   instance_type               = var.instance_type
   associate_public_ip_address = true
-  key_name                    = "SSH"
+  key_name                    = "******"
   subnet_id                   = aws_subnet.subnet_public_web_haproxy.id
   vpc_security_group_ids      = [aws_security_group.web_haproxy.id]
   depends_on                  = [aws_instance.ec2_web_server]
 
   user_data = templatefile("haproxy.tpl", {
-    instance_ip   = join(",", aws_instance.ec2_web_server.*.private_ip)
+    instance_ip   = join(",", aws_instance.ec2_web_server.*.public_ip)
     instance_name = join(",", aws_instance.ec2_web_server.*.tags.Name)
   })
 
@@ -28,8 +28,7 @@ resource "aws_instance" "ec2_web_server" {
   key_name                    = "SSH"
   subnet_id                   = aws_subnet.subnet_public_web_haproxy.id
   vpc_security_group_ids      = [aws_security_group.web_haproxy.id]
-
-  user_data = templatefile("web_server.tpl", {})
+  user_data                   = templatefile("web_server.tpl", {})
 
   tags = {
     Name = "web_${format("%02d", count.index + 1)}"

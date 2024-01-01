cbd4dd4c0a385b9dcec2099d2c835bfdbcfebb33
Dima Dolgov <90906120+dimadolgov@users.noreply.github.com>
2024-01-01 09:24:54 +0200

    Update 03-ec2.tf

--- 03-ec2.tf
+++ 03-ec2.tf
@@ -9,7 +9,7 @@ resource "aws_instance" "ec2_haproxy" {
   vpc_security_group_ids      = [aws_security_group.web_haproxy.id]
   depends_on                  = [aws_instance.ec2_web_server] # Ensure the web server instances are created first
 
-  user_data = templatefile("haproxy.tpl", {
+  user_data = templatefile("06-haproxy.tpl", {
     instance_ip   = join(",", aws_instance.ec2_web_server.*.public_ip)
     instance_name = join(",", aws_instance.ec2_web_server.*.tags.Name)
   })
@@ -28,7 +28,7 @@ resource "aws_instance" "ec2_web_server" {
   key_name                    = "******" # Specify your actual key pair name
   subnet_id                   = aws_subnet.subnet_public_web_haproxy.id
   vpc_security_group_ids      = [aws_security_group.web_haproxy.id]
-  user_data                   = templatefile("web_server.tpl", {}) # You may provide additional parameters here
+  user_data                   = templatefile("07-web_server.tpl", {}) # You may provide additional parameters here
 
   tags = {
     Name = "web_${format("%02d", count.index + 1)}"
